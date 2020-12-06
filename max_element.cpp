08fc0ec468e518ef836409cb41b7dda641e87911
Puneet Mishra <33493449+Puneetm146@users.noreply.github.com>
2020-12-06 22:03:38 +0530

    Done few changes
    done

--- max_element.cpp
+++ max_element.cpp
@@ -7,15 +7,15 @@ int main(){
 	vector <int> v {10,20,100,40,50,500};
 	int max;
 	
-	/*for(int i=0; i<v.size(); i++){
+	for(int i=0; i<v.size(); i++){
 		for(int j=0; j<v.size(); j++){
 			if(v[i] > v[j]){
 				max = v[i];	
 			}
 		}
-	}*/
+	}
 	
-	max = *max_element(v.begin(),v.end());	
+//	max = *max_element(v.begin(),v.end());	
 	
 	cout << "max element " << max << endl;
 	
